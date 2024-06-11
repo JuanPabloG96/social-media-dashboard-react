c81103413c6e6d012007955d37e804351fa4c008
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-06-11 02:15:47 -0600

    testing: media card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { MediaCard } from "./components/MediaCard"
+import { OverviewCard } from "./components/OverviewCard"
+
 export function App() {
   return (
     <>
-      <h1 className="text-3xl">Hola Mundo!</h1>
+      <section className="mx-auto my-4 w-[100%] max-w-[750px] flex flex-col gap-10 items-center">
+        <MediaCard
+          title={"FOLLOWERS"}
+          socialMediaName={"facebook"}
+          imageUrl={"images/icon-facebook.svg"}
+          username={"Juan Pablo"}
+          count={"2351"}
+          status={"1025 Today"}
+          statusIcon={"images/icon-up.svg"}
+        />
+      </section>
     </>
   )
 }
